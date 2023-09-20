471f2e8690dbb912c4175b5c3a25abf5ac99e0e2
Attila Szabó <lindi.2brothers@gmail.com>
2023-09-20 19:15:13 +0200

    rock paper scissors game initial logic

--- python.py
+++ python.py
@@ -1,1 +1,23 @@
-print("Hello Word");
+import random
+options = ["kő","papír","olló"];
+computer_points = 0;
+player_ponts = 0;
+
+while True:    
+    player = input("Kérlek válassz a következők közül: Kő, Papír, Olló\n :").lower();
+    computer = random.choice(options)
+    if player in options:
+        if player == computer:
+            print("A játék döntetlen \n Újból!");
+        elif (
+            (player == "kő" and computer == "olló") or
+            (player == "papír" and computer == "kő") or
+            (player == "olló" and computer == "papír")
+        ):
+            print("játékos nyert!");
+            break;
+        else:
+            print("A számítóép nyert!");
+            break;
+    else :
+        print("Helytelen opciót adtál meg!")
