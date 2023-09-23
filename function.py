4bb84969656bed378f32f24526468f66488a7b44
Attila Szabó <lindi.2brothers@gmail.com>
2023-09-23 19:16:26 +0200

    Rock paper scissors game, for scores.

--- function.py
+++ function.py
@@ -3,12 +3,17 @@ options = ["rock","paper","scissors"];
 
 def run_game (x):
     while True:
+        player_score = 0;
+        computer_score = 0;
         player = input("Please choose from: Rock, Paper, Scissors: ").lower();
         computer = random.choice(x);
         if player in x:
             winner = game_evaluation (player, computer);
             print (winner);
             if winner != "The game is a draw!":
+                score (winner,player_score,computer_score)
+                winner_score (player_score,computer_score)
+            if player_score == "3" or computer_score == "3":
                 break;
         else:
             print("Bad choice!")
@@ -25,7 +30,17 @@ def game_evaluation (player, computer):
     else:
         return "The computer won!"
 
+def score (w,p,c):
+    if w == "The player Won!":
+        p+=1;
+    else:
+        c+=1;
 
-    
+def winner_score (p_s,c_s):
+    if p_s == "3":
+        print("Players its winner")
+    elif c_s == "3":
+        print("Computer its winner")
+
 run_game(options)
 
---
+++ points.py
@@ -0,0 +1,44 @@
+import random 
+
+def rock_paper_scissors():
+    options = ["rock","paper","scissors"]
+    round = True;
+    player_score = 0;
+    computer_score = 0;
+    while round:
+        player = input("Please choose: Rock, Paper, Scissors\n: ").lower();
+        computer = random.choice(options);
+        if player in options:
+            player_score, computer_score = score(player,computer,player_score,computer_score)
+            round = ultimate_winner(player_score,computer_score,round)
+        else:
+            print("please enter a correct value!")
+
+
+def score(player,computer,player_score,computer_score):
+    if player == computer:
+        print("The game is a draw!");
+    elif (
+        (player == "rock" and computer == "scissors") or
+        (player == "paper" and computer == "rock")  or
+        (player == "scissors" and computer == "paper")
+    ):
+        player_score +=1;
+        print(f"Player won! \n The score is: {player_score}");
+    else:
+        computer_score +=1;
+        print(f"Computer won! \n The score is: {computer_score}");
+    return player_score, computer_score
+    
+def ultimate_winner(j_pont,sz_pont,round):
+    if j_pont == 3:
+        print("The player won the round!");
+        return False;
+    elif sz_pont == 3:
+        print("The computer won the round!");
+        return False;
+    else:
+        return True;
+
+
+rock_paper_scissors()
