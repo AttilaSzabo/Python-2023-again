eeaebad4d1752e9910b75f6ca98294797c514d03
Attila Szabó <lindi.2brothers@gmail.com>
2023-09-24 13:35:16 +0200

    Further development

--- bank_account.py
+++ bank_account.py
@@ -1,15 +1,15 @@
 
 def enter_password():
-    password = "123"
+    password = "123";
     correct_input = True;
     right_try = 0;
     while correct_input:
         user = input("Please enter your password: ");
         if user == password:
             print("The password is correct!");
-            choice_options()
-            print("")
-            correct_input = False
+            choice_options();
+            print("");
+            correct_input = False;
         elif user != password:
             print("The password is incorrect!");
             print("")
@@ -18,29 +18,53 @@ def enter_password():
                 print("The card has been reserved, please report to the bank branch!");
                 correct_input = False;
 
+
 def choice_options():
-    account_balance = 1000.50;
+    account_balance = 1000.00;
     options = {
-        "Check your balance":1,
-        "Cash withdrawal":2,
-        "account_balance": 3
+        "Cash withdrawal":1,
+        "Check your balance":2
         }
-    print("")
+    print("");
     print(f"Choose from the following options:")
-    print("")
+    print("");
     for x, y in options.items():
         print(f"{x}, button {y}")
     while True:
         try:
             user = int(input("Enter number: "));
             if user > 3:
-                print("Please enter correctly value!")
+                print("Please enter correctly value!");
             else:
-                print("Igen helyes a bemenet!")
+                selected_options(user,account_balance);
                 break;
         except ValueError:
-            print("")
-            print("Please enter correctly value!")
+            print("");
+            print("Please enter correctly value!");
 
+
+def selected_options(user,balance):
+    if user == 1:
+        money_withdrawal(balance);
+    elif user == 2:
+        print(f"Your account balance: {balance:.2f} €");
+
+
+def money_withdrawal(money):
+    correct_input = True;
+    while correct_input:
+        try:
+            user = int(input("Enter the amount: "));
+            if user > money:
+                print("You don't have that much money in your account!");
+            elif user % 5 != 0:
+                print("You cannot withdraw less than 5 euros!");
+            else:
+                result = f"{money - user:.2f}"
+                print(f"Amount withdrawn: {user}");
+                print(f"You have {result} € amounts left on your account!");
+                correct_input = False;
+        except ValueError:
+            print("Please enter correctly value!");
 
 enter_password()
